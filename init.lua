0edcc89b3fd407cfa20b576c24820adfde510eb7
andreiDiazSimon <andreisimon2004@gmail.com>
2025-02-26 05:53:15 +0800

    add option undofile, wrap and few highlights

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 vim.g.mapleader = " "
 require("config.lazy")
 vim.g.netrw_liststyle = 2
--- transparent cursorLine highlight background
-vim.cmd [[ highlight CursorLine guibg=NONE ]]
 vim.keymap.set('n', '<leader>e', function()
 	if vim.bo.filetype == 'netrw' then
 		vim.cmd(':bd')
@@ -14,35 +12,19 @@ end, { noremap = true, silent = true })
 vim.keymap.set('n', '<C-s>', function()
 	vim.cmd(':w')
 end, { noremap = true, silent = true })
-
-
-vim.keymap.set('n', '<S-h>', function()
-	vim.cmd(':tabp')
-end, { noremap = true, silent = true })
-vim.keymap.set('n', '<S-l>', function()
-	vim.cmd(':tabn')
-end, { noremap = true, silent = true })
-
 vim.keymap.set('i', '<C-s>', function()
 	vim.cmd(':w')
 	vim.api.nvim_input('<Esc>')
 end, { noremap = true, silent = true })
+
 vim.keymap.set('n', 'vag', 'ggVG', { noremap = true, silent = true, desc = "Select entire file" })
 vim.keymap.set('n', 'ga', 'ggVGy', { noremap = true, silent = true, desc = "Select entire file and copy" })
 
 
-vim.keymap.set('n', 'gj', '<C-d>', { noremap = true, silent = true, desc = "scroll down" })
-vim.keymap.set('n', 'gk', '<C-u>', { noremap = true, silent = true, desc = "scroll up" })
-vim.keymap.set('n', 'gl', '$', { noremap = true, silent = true, desc = "go utmost right" })
-vim.keymap.set('n', 'gh', '0', { noremap = true, silent = true, desc = "go utmost left" })
-
---vim.g.maplocalleader = "\\"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = 'a'
-
--- you set it to a very large value (999) the cursor line will always be in the middle of the window (except at the start or end of the file or	when long lines wrap)
-vim.opt.scrolloff = 999
+vim.opt.wrap = true
 
 -- Use the system clipboard for all yank, delete, change and paste operations
 vim.opt.clipboard = 'unnamedplus'
@@ -55,11 +37,14 @@ vim.opt.smartcase = true
 -- cursor to stay in block mode
 vim.opt.guicursor = ""
 
+vim.opt.undofile = true                               -- Enable persistent undo and redo
+vim.opt.undodir = vim.fn.stdpath("config") .. "/undo" -- Directory for undo files
+
 vim.keymap.set('n', '<leader>l', ':Lazy show<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>m', ':Mason<CR>', { noremap = true, silent = true })
 
 -- Yank selected text to clipboard when pressing y in visual mode
-vim.keymap.set('v', 'y', '"+y', { noremap = true })
+-- vim.keymap.set('v', 'y', '"+y', { noremap = true })
 
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', { noremap = true, silent = true })
@@ -69,8 +54,23 @@ vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', { noremap = true, silent = true })
 vim.keymap.set('i', 'jk', '<Esc>l', { noremap = true, silent = true })
 vim.keymap.set('i', 'kj', '<Esc>', { noremap = true, silent = true })
 
-vim.opt.undofile = true                      -- Enable persistent undo and redo
-vim.opt.undodir = vim.fn.stdpath("config") .. "/undo" -- Directory for undo files
 
 
-vim.keymap.set('n', '<leader>sh', ':!./%\n', {noremap = true, silent = true , desc = "execute current bash script"})
+-- transparent cursorLine highlight background
+vim.cmd [[ highlight CursorLine guibg=NONE ]]
+
+-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+
+-- highight sa float fg and bg color
+vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#5a5b5e", bg = "none" })
+-- highight sa float border fg and bg color
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#5a5b5e", bg = "none" })
+
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#7c5c20", bg = "none" })
+
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#833b3b", bg = "none" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#2b5d63", bg = "none" })
+
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#79428a", bg = "none" })
+
+vim.keymap.set('n', '<leader>sh', ':!./%\n', { noremap = true, silent = true, desc = "execute current bash script" })
